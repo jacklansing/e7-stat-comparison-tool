1f8c9949f194876aa0ef6ab10f7616d88c6e232d
Jack Lansing <john.a.lansing@gmail.com>
2020-03-17 10:20:28 -0400

    cleaned up boiler, installed gatsby-transformer-jso, added started component for proof of concept

---
+++ gatsby-test/gatsby-site/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import HeroCard from '../components/hero-card';
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>E7 Hero Comparison</h1>
+  </Layout>
+);
+
+export default IndexPage;
