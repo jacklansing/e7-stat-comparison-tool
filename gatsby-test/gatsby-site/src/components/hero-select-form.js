d510388cb2c63877199aa2c43e676fd99798a8e9
Jack Lansing <john.a.lansing@gmail.com>
2020-03-17 14:46:28 -0400

    basic form styling

--- gatsby-test/gatsby-site/src/components/hero-select-form.js
+++ gatsby-test/gatsby-site/src/components/hero-select-form.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import FormValidationError from './form-validation-error';
+import { css } from '@emotion/core';
 
 const HeroSelectForm = ({ heroes, onSetHero, className }) => {
   const [name, setName] = useState('');
@@ -9,10 +10,52 @@ const HeroSelectForm = ({ heroes, onSetHero, className }) => {
     onSetHero(selected);
   }
 
+  const radioStyles = css`
+    appearance: none;
+  `;
+
+  const selectedRadioStyle = css`
+    background-color: darkblue;
+    color: white;
+  `;
+
+  const radioLabelStyles = css`
+    border: 1px solid black;
+    border-radius: 5px;
+    box-shadow: 1px 3px 1px lightgrey;
+    display: block;
+    margin-top: 1rem;
+    max-width: 50vw;
+    padding: 1rem;
+  `;
+
+  const textLabelStyles = css`
+    margin-right: 0.45rem;
+    display: block;
+  `;
+
+  const textInputStyles = css`
+    padding: 0.45rem;
+    border: 1px solid gray;
+    box-shadow: 1px 3px 3px lightgrey;
+    border-radius: 5px;
+  `;
+
+  const formStyles = css`
+    font-family: sans-serif;
+  `;
+
   return (
-    <form className={className} onSubmit={e => e.preventDefault()}>
-      <label htmlFor={className + '-name'}>Hero Name: </label>
+    <form
+      css={formStyles}
+      className={className}
+      onSubmit={e => e.preventDefault()}
+    >
+      <label css={textLabelStyles} htmlFor={className + '-name'}>
+        Hero Name:{' '}
+      </label>
       <input
+        css={textInputStyles}
         type="text"
         id={className + '-name'}
         name={className + '-name'}
@@ -25,17 +68,22 @@ const HeroSelectForm = ({ heroes, onSetHero, className }) => {
         heroes
           .filter(hero => hero.includes(name))
           .map(hero => (
-            <div key={hero}>
-              <label>
-                <input
-                  type="radio"
-                  name="selected-hero"
-                  value={hero}
-                  onChange={e => setSelected(e.target.value)}
-                />
-                {hero}
-              </label>
-            </div>
+            <label
+              key={hero}
+              css={css`
+                ${radioLabelStyles};
+                ${hero === selected ? selectedRadioStyle : ''};
+              `}
+            >
+              <input
+                css={radioStyles}
+                type="radio"
+                name="selected-hero"
+                value={hero}
+                onChange={e => setSelected(e.target.value)}
+              />
+              {hero}
+            </label>
           ))}
     </form>
   );
