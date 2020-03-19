1481a08855f9fb8ce619e2918518fd501bebc513
Jack Lansing <john.a.lansing@gmail.com>
2020-03-19 18:32:27 -0400

    refactor stat comparison into StatCompare component

--- src/components/hero-comparison.js
+++ src/components/hero-comparison.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { css } from '@emotion/core';
+import StatCompare from './stat-compare';
 
 const HeroComparison = ({ heroOne, heroTwo }) => {
   /* Commented out stats that are always the same. For now they are
@@ -82,87 +83,48 @@ const HeroComparison = ({ heroOne, heroTwo }) => {
         </p>
       ) : (
         <ul css={comparisonListStyles}>
-          {spdHeroOne > spdHeroTwo ? (
-            <li>
-              <span>{nameHeroOne}</span> has{' '}
-              <span css={stat}>{spdHeroOne - spdHeroTwo}</span> more base speed
-            </li>
-          ) : (
-            <li>
-              <span>{nameHeroTwo}</span> has{' '}
-              <span css={stat}>{spdHeroTwo - spdHeroOne}</span> more base speed
-            </li>
-          )}
-
-          {atkHeroOne > atkHeroTwo ? (
-            <li>
-              <span>{nameHeroOne}</span> has{' '}
-              <span css={stat}>{atkHeroOne - atkHeroTwo}</span> more base attack
-            </li>
-          ) : (
-            <li>
-              <span>{nameHeroTwo}</span> has{' '}
-              <span css={stat}>{atkHeroTwo - atkHeroOne}</span> more base attack
-            </li>
-          )}
-
-          {chcHeroOne > chcHeroTwo ? (
-            <li>
-              <span>{nameHeroOne}</span> has{' '}
-              <span css={stat}>{chcHeroOne * 100 - chcHeroTwo * 100}%</span>{' '}
-              more base crit rate
-            </li>
-          ) : (
-            <li>
-              <span>{nameHeroTwo}</span> has{' '}
-              <span css={stat}>{chcHeroTwo * 100 - chcHeroOne * 100}%</span>{' '}
-              more base crit rate
-            </li>
-          )}
-
-          {chdHeroOne !== chdHeroTwo ? (
-            <>
-              {chdHeroOne > chdHeroTwo ? (
-                <li>
-                  <span>{nameHeroOne}</span> has{' '}
-                  <span css={stat}>{chdHeroOne * 100 - chdHeroTwo * 100}%</span>{' '}
-                  more base crit damage
-                </li>
-              ) : (
-                <li>
-                  <span>{nameHeroTwo}</span> has{' '}
-                  <span css={stat}>{chdHeroTwo * 100 - chdHeroOne * 100}%</span>{' '}
-                  more base crit damage
-                </li>
-              )}
-            </>
-          ) : null}
-
-          {hpHeroOne > hpHeroTwo ? (
-            <li>
-              <span>{nameHeroOne}</span> has{' '}
-              <span css={stat}>{hpHeroOne - hpHeroTwo}</span> more base health
-            </li>
-          ) : (
-            <li>
-              <span>{nameHeroTwo}</span> has{' '}
-              <span css={stat}>{hpHeroTwo - hpHeroOne}</span> more base health
-            </li>
-          )}
-
-          {defHeroOne > defHeroTwo ? (
-            <li>
-              <span>{nameHeroOne}</span> has{' '}
-              <span css={stat}>{defHeroOne - defHeroTwo}</span> more base
-              defense
-            </li>
-          ) : (
-            <li>
-              <span>{nameHeroTwo}</span> has{' '}
-              <span css={stat}>{defHeroTwo - defHeroOne}</span> more base
-              defense
-            </li>
-          )}
+          <StatCompare
+            statType="spd"
+            heroOneStat={spdHeroOne}
+            heroTwoStat={spdHeroTwo}
+            heroOneName={nameHeroOne}
+            heroTwoName={nameHeroTwo}
+          />
+          <StatCompare
+            statType="atk"
+            heroOneStat={atkHeroOne}
+            heroTwoStat={atkHeroTwo}
+            heroOneName={nameHeroOne}
+            heroTwoName={nameHeroTwo}
+          />
+          <StatCompare
+            statType="chc"
+            heroOneStat={chcHeroOne}
+            heroTwoStat={chcHeroTwo}
+            heroOneName={nameHeroOne}
+            heroTwoName={nameHeroTwo}
+          />
+          <StatCompare
+            statType="chd"
+            heroOneStat={chdHeroOne}
+            heroTwoStat={chdHeroTwo}
+            heroOneName={nameHeroOne}
+            heroTwoName={nameHeroTwo}
+          />
+          <StatCompare
+            statType="hp"
+            heroOneStat={hpHeroOne}
+            heroTwoStat={hpHeroTwo}
+            heroOneName={nameHeroOne}
+            heroTwoName={nameHeroTwo}
+          />
+          <StatCompare
+            statType="def"
+            heroOneStat={defHeroOne}
+            heroTwoStat={defHeroTwo}
+            heroOneName={nameHeroOne}
+            heroTwoName={nameHeroTwo}
+          />
         </ul>
       )}
     </>
